3794a45b6352cfcd7630f4b0e92cc3646ade934f
Văn Bang <toanhocolympic@gmail.com>
2023-04-23 16:08:54 +0000

    cách khác vd 1

--- main.js
+++ main.js
@@ -26,13 +26,13 @@ let courses = [
 	}
 ];
 
-// let student = new Array(
-//     new Array("Nguyễn Văn Bang", 21, 9.0),
-//     new Array("Nguyễn Lê Duy Đạt", 21, 8.5),
-//     new Array("Phan Thanh Nhi", 21, 9.5),
-//     new Array("Nguyễn Thị Thu Phương", 21, 8.0),
-//     new Array("Võ Bảo Vi", 21, 9.0)  
-//   );
+let students = new Array(
+    new Array("Nguyễn Văn Bang", 21, 9.0),
+    new Array("Nguyễn Lê Duy Đạt", 21, 8.5),
+    new Array("Phan Thanh Nhi", 18, 9.5),
+    new Array("Nguyễn Thị Thu Phương", 19, 8.0),
+    new Array("Võ Bảo Vi", 21, 9.0)  
+  );
   
 //   document.addEventListener("DOMContentLoaded", function(){
 //   const datatable = document.getElementById("datatable");
@@ -52,40 +52,75 @@ let courses = [
 // var age = document.getElementsByClassName("age");
 // var dtb = document.getElementsByClassName("dtb");
 
+// const nodepara = document.getElementById("node_para");
+// var result = "";
+
+// let ListCourses = courses.filter(function (course, index) {
+// 	return course.age >= 20;
+// });
+
+// ListCourses.forEach(function(course, index) {
+// 	var stringresult = `
+// 	<tr>
+// 		<th scope="col">${index + 1}</th>
+// 		<th scope="col">${course.name}</th>
+// 		<th scope="col">${course.age}</th>
+// 		<th scope="col">${course.dtb}</th>
+//     </tr>
+// 	`;
+// 	result += stringresult;
+// 	nodepara.innerHTML = result;
+
+//     // const para = document.createElement("tr");
+//     // const nodepara = document.getElementById("node_para");
+//     // nodepara.appendChild(para);
 
 
-let ListCourses = courses.filter(function (course, index) {
-	return course.age >= 20;
+//     // const thchild = document.createElement("td");
+//     // const nodeth = document.createTextNode(`${index + 1}`);
+//     // thchild.appendChild(nodeth);
+
+//     // const tdnamechild = document.createElement("td");
+//     // const nodetdname = document.createTextNode(`${course.name}`);
+//     // tdnamechild.appendChild(nodetdname);
+
+
+//     // const tdagechild = document.createElement("td");
+//     // const nodetdage = document.createTextNode(`${course.age}`);
+//     // tdagechild.appendChild(nodetdage);
+
+//     // const tddtbchild = document.createElement("td");
+//     // const nodedtdtb = document.createTextNode(`${course.dtb}`);
+//     // tddtbchild.appendChild(nodedtdtb);
+
+//     // para.appendChild(thchild);
+//     // para.appendChild(tdnamechild);
+//     // para.appendChild(tdagechild);
+//     // para.appendChild(tddtbchild);
+    
+// })
+
+// nodepara.innerHTML = result;
+const nodepara = document.getElementById("node_para");
+var result = "";
+
+
+let ListStudent = students.filter(function (student, index) {
+	return student[1] >= 20;
 });
 
-ListCourses.forEach(function(course, index) {
-   
-    const para = document.createElement("tr");
-    const nodepara = document.getElementById("node_para");
-    nodepara.appendChild(para);
-
-
-    const thchild = document.createElement("td");
-    const nodeth = document.createTextNode(`${index + 1}`);
-    thchild.appendChild(nodeth);
-
-    const tdnamechild = document.createElement("td");
-    const nodetdname = document.createTextNode(`${course.name}`);
-    tdnamechild.appendChild(nodetdname);
-
-
-    const tdagechild = document.createElement("td");
-    const nodetdage = document.createTextNode(`${course.age}`);
-    tdagechild.appendChild(nodetdage);
-
-    const tddtbchild = document.createElement("td");
-    const nodedtdtb = document.createTextNode(`${course.dtb}`);
-    tddtbchild.appendChild(nodedtdtb);
-
-    para.appendChild(thchild);
-    para.appendChild(tdnamechild);
-    para.appendChild(tdagechild);
-    para.appendChild(tddtbchild);
-    
-    
+ListStudent.forEach(function(student, index) {
+	console.log(student);
+	var stringresult = `
+	<tr>
+		<th scope="col">${index + 1}</th>
+		<th scope="col">${student[0]}</th>
+		<th scope="col">${student[1]}</th>
+		<th scope="col">${student[2]}</th>
+    </tr>
+	`;
+	result += stringresult;
+	nodepara.innerHTML = result;   
 })
+
+// nodepara.innerHTML = result;
