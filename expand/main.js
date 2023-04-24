6a0ee6251eb3a68799a49c310a1edf7821843ea2
Văn Bang <toanhocolympic@gmail.com>
2023-04-24 17:45:51 +0000

    cách khác của btap mở rộng

--- expand/main.js
+++ expand/main.js
@@ -12,23 +12,33 @@ btadd.addEventListener("click", function(){
 
 let tobj = new Array (count, hoten.value, tuoi.value, mau.value);
 data.push(tobj);
-let tnode = document.createElement("button");
-tnode.setAttribute("id", count);
-tnode.setAttribute("class", "btn m-3");
-tnode.setAttribute("style", "color: white");
+
+// let tnode = document.createElement("button");
+// tnode.setAttribute("id", count);
+// tnode.setAttribute("class", "btn m-3");
+// tnode.setAttribute("style", "color: white");
 if(hoten.value == "") {
     window.alert("Hãy nhập tên !");
     return;
-} else if (tuoi.value == 0 || mau.value == backgroundColor.) {
+} else if (tuoi.value == 0) {
     window.alert("Hãy nhập tuổi !");
     return;
 } else {
-    tnode.textContent = `${hoten.value} có tuổi là ${tuoi.value}`;
+    let value = `
+    <button id="${count}" class="btn m-3" style="color: white; background-color: ${mau.value};">
+    ${hoten.value} có tuổi là ${tuoi.value}
+    </button>
+    `;
+    result.innerHTML += value;
+    count++;
 }
 
-tnode.style.backgroundColor = mau.value;
-tnode.addEventListener("click", function(){
-    let id = this.id;
+let tnode = document.querySelectorAll("button");
+console.log(tnode);
+var mylistnode = Array.from(tnode)
+mylistnode.forEach(element => {
+    element.addEventListener("click", function() {
+        let id = this.id;
     let index = 0;
     for(let i=0; i<data.length; i++)
     if(data[i] [0] == id){
@@ -38,10 +48,25 @@ tnode.addEventListener("click", function(){
     hoten.value = data[index] [1];
     tuoi.value = data[index] [2];
     mau.value = data[index] [3];
+    })
 });
+
+// tnode.style.backgroundColor = mau.value;
+// tnode.addEventListener("click", function(){
+//     let id = this.id;
+//     let index = 0;
+//     for(let i=0; i<data.length; i++)
+//     if(data[i] [0] == id){
+//         index = i;
+//         break;
+//     }
+//     hoten.value = data[index] [1];
+//     tuoi.value = data[index] [2];
+//     mau.value = data[index] [3];
+// });
 //tuoi.addEventListener("")
-result.appendChild(tnode)
-count++;
+// result.appendChild(tnode)
+
 });
 btcancel.addEventListener("click", function(){
     hoten.value = "";
